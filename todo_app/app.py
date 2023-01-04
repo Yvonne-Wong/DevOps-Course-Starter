cf3da1d8f63c463a5c930be8db7abed25a962abe
Yvonne Wong <yvonne.wong@softwire.com>
2023-01-04 23:09:02 +0000

    add integration tests, refactor trello_items.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,33 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data.classes.viewmodel import ViewModel
 import todo_app.data.trello_items as trello_items
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
+    @app.route('/')
+    def index():
+        items = trello_items.get_items()
+        lists = trello_items.get_lists()
+        view_model = ViewModel(items, lists)
+        return render_template('index.html', items=items, lists=lists)
 
-@app.route('/')
-def index():
-    items = trello_items.get_items()
-    lists = trello_items.get_lists()
-    return render_template('index.html', items=items, lists=lists)
+    @app.route('/', methods=['POST'])
+    def create_todo():
+        trello_items.add_todo(request.form.get('todo'))
+        return redirect(url_for('index'))
 
-@app.route('/', methods=['POST'])
-def create_todo():
-    trello_items.add_todo(request.form.get('todo'))
-    return redirect(url_for('index'))
+    @app.route('/start/<id>', methods=['POST'])
+    def start_item(id: str):
+        trello_items.start_item(id)
+        return redirect(url_for('index'))
 
-@app.route('/start/<id>', methods=['POST'])
-def start_item(id: str):
-    trello_items.start_item(id)
-    return redirect(url_for('index'))
+    @app.route('/complete/<id>', methods=['POST'])
+    def complete_item(id: str):
+        trello_items.complete_item(id)
+        return redirect(url_for('index'))
 
-@app.route('/complete/<id>', methods=['POST'])
-def complete_item(id: str):
-    trello_items.complete_item(id)
-    return redirect(url_for('index'))
+    @app.route('/undo/<id>', methods=['POST'])
+    def undo_item(id: str):
+        trello_items.undo_item(id)
+        return redirect(url_for('index'))
 
-@app.route('/undo/<id>', methods=['POST'])
-def undo_item(id: str):
-    trello_items.undo_item(id)
-    return redirect(url_for('index'))
+    return app
